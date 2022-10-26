578ed8bef175c23c30e2a48555391793bd81b4fb
Tina Gosarič <tina.gosaric@gorenje.com>
2022-10-26 13:57:57 +0200

    Adding exampÄles.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SomeClass one = new SomeClass("one");
+        SomeClass two = new SomeClass("two");
+        SomeClass three = new SomeClass("three");
+
+        System.out.println(one.getInstanceNumber());
+        System.out.println(two.getInstanceNumber());
+        System.out.println(three.getInstanceNumber());
+
+        int pass = 5645;
+        Password password = new ExtendedPassword(pass);
+        password.storePassword();
+        password.letMeIn(1);
+        password.letMeIn(4545);
+        password.letMeIn(534454);
+        password.letMeIn(5645);
     }
 }
